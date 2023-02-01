ad20baea9ee6945c5b9861ccd82fbb1f53976794
Sohel <absohel47@gmail.com>
2023-02-01 01:48:06 +0000

    Creating a Model

--- status/models.py
+++ status/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+
+# Image Upload Function
+def upload_status_image(instance, filename):
+    return f"uploads/{instance.user}/{filename}"
+
 
 # Create your models here.
+class Status(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to=upload_status_image, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.content)[0:30]
+
+    class Meta:
+        verbose_name_plural = "Status List"
